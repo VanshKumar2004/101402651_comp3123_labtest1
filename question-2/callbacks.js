54b74d6350cef0e5b69fda2b89674645da392007
Vansh Kumar <vanshpalyani@gmail.com>
2024-10-03 16:00:57 -0400

    lab test 1

---
+++ question-2/callbacks.js
@@ -0,0 +1,18 @@
+const resolvedPromise = () =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve({ message: "delayed success!" }), 500);
+    });
+  
+  const rejectedPromise = () =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => reject({ error: "delayed exception!" }), 500);
+    });
+  
+resolvedPromise()
+.then((message) => console.log(message))
+.catch((error) => console.error(error)); 
+
+rejectedPromise()
+.then((message) => console.log(message))
+.catch((error) => console.error (error));
+
